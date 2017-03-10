11bd228e89e15859b3f83f90f1e2a22d373f9643
estuine <vorco117@gmail.com>
2017-03-10 11:25:31 -0800

    HW8
    target with sounds and counter

---
+++ Assets/Scripts/target.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour {
+
+    public AudioClip toPlay;
+    public float lagTime;
+
+    private int hitcounter=0;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = toPlay;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "bullet")
+        {
+            hitcounter++;
+            StartCoroutine(playSound());
+            
+             
+        }
+    }
+
+    private IEnumerator playSound()
+    {
+        for (int i = 0; i < hitcounter; i++)
+        {
+            print("hit " + (i + 1) + " " + hitcounter + " total hits");
+            GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(lagTime);
+        }
+    }
+}
